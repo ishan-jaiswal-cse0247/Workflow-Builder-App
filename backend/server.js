bbb10c61b906abfb9b904f25a9b69a9f36bbb5a0
Ishan Jaiswal <ashu.aa79@gmail.com>
2024-05-10 14:08:29 +0530

    init-commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -8,12 +9,21 @@ app.use(require('./routes/router'));
 dotenv.config({ path: './.env' });
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 //for connect MongoDB server
 const Connect = async () => {
   await mongoose
     .connect(process.env.URI)
     .then(() => {
-      console.log('connected..');
+      console.log('connected');
     })
     .catch((err) => {
       console.log(err);
