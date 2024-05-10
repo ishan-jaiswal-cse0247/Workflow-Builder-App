b8d602c723f7031788f0f6a1879f6458e99350b2
Ishan Jaiswal <ashu.aa79@gmail.com>
2024-05-10 15:18:17 +0530

    final-commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 //const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 );
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
